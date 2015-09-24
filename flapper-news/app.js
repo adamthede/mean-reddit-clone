9644321ddf0861bda60faac432a97d7d8d13eeed
Adam Thede <adam@thedetech.com>
2015-09-24 16:23:30 -0500

    wired up and authenticating

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -8,8 +8,13 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+
+require('./config/passport');
 
 // connect to mongoose
 mongoose.connect('mongodb://localhost/news', function(err, db){
@@ -34,6 +39,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
